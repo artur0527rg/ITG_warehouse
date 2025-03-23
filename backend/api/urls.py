0224b419d7bbb3c9c83e110205eae918fc6273b4
artur0527rg <rogozinskiyartur@gmail.com>
2025-03-23 18:37:02 +0100

    JWT auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+  TokenObtainPairView, TokenRefreshView, TokenVerifyView,
+)
+
 from api.views import (
   ZoneViewSet, LineViewSet, PlaceViewSet, OrderViewSet, PalletViewSet,
 )
@@ -12,5 +16,8 @@ router.register(r'orders', OrderViewSet)
 router.register(r'pallets', PalletViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+  path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+  path('', include(router.urls)),
 ]
