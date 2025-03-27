106615bea883ce9f349539fb678cbf4983303f21
artur0527rg <rogozinskiyartur@gmail.com>
2025-03-27 09:34:15 +0100

    CORS middleware

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 
 # Application definition
 
@@ -49,6 +54,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # Third party
+    'corsheaders.middleware.CorsMiddleware',
+    # My middlewares
+    # Default
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
