8c88648a749aa9ab2f953c14751e0b5531700d82
artur0527rg <rogozinskiyartur@gmail.com>
2025-03-27 11:54:06 +0100

    Sidebar

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,17 +1,20 @@
 import  AuthProvider  from "./contexts/AuthProvider";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Login from "./routes/Login";
-import Board from "./routes/Board";
+import Warehouse from "./routes/Warehouse";
+import Orders from "./routes/Orders";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route element={<PrivateRoutes/>}>
-          <Route path="/" element={<Board/>}/>
-        </Route>
         <Route path="/login" element={<Login/>} />
+        <Route element={<PrivateRoutes/>}>
+          <Route path="/" element={<Navigate to={'/board'}/>}/>
+          <Route path="/board" element={<Warehouse/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+        </Route>
       </Routes>
     </AuthProvider>
   );
---
+++ frontend_react/src/routes/Warehouse.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/SideBar";
+import './warehouse.css'
+
+const Warehouse = () => {
+  return (
+    <div className="app">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Warehouse;
