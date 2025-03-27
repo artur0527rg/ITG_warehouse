3ff8221cdcb09bc7da6114a5a6ccfb9fb0d2e91e
artur0527rg <rogozinskiyartur@gmail.com>
2025-03-27 06:56:37 +0100

    react forntend

---
+++ frontend_react/src/App.jsx
@@ -0,0 +1,12 @@
+import { AuthProvider } from './contexts/authContext'
+
+function App() {
+
+  return (
+    <AuthProvider>
+      
+    </AuthProvider>
+  )
+}
+
+export default App
---
+++ frontend_react/src/contexts/authContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [accessToken , setAccessToken] = useState(localStorage.getItem('access'));
+  const [refreshToken, setRefreshToken] = useState(localStorage.getItem('refresh'));
+
+  useEffect(() => {
+    if (accessToken) {
+      localStorage.setItem('access', accessToken);
+    } else {
+      localStorage.removeItem('access');
+    }
+    
+    if (refreshToken) {
+      localStorage.setItem('refresh', refreshToken);
+    } else {
+      localStorage.removeItem('refresh');
+    }
+  }, [accessToken, refreshToken])
+
+  const contextValue = {
+    accessToken,
+    setAccessToken,
+    refreshToken,
+    setRefreshToken,
+  }
+
+  return <AuthContext.Provider value={contextValue}>
+    {children}
+  </AuthContext.Provider>
+};
+
+export const useAuth = () => useContext(AuthContext)
